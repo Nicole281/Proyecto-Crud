4980ce522ffda456e3cc33f3f4f014d89e16dbbd
Nicole281 <nicole.nasg@gmail.com>
2024-09-17 16:59:13 -0300

    se crea una funcion para registrar nuevo usuario

--- main/services.py
+++ main/services.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
 from django.db.models import Q
 from django.db import connection
+from django.contrib import messages
+
 
 
 
@@ -45,9 +47,9 @@ def editar_inmueble(inmueble_id:int, nombre:str, descripcion:str, m2_construidos
     return True
 
 
-def crear_user(username:str, first_name:str, last_name:str, email:str, password:str, pass_confirm:str, direccion:str, rol:str='arrendatario', telefono:str=None) -> bool:
+def crear_user(request, username:str, first_name:str, last_name:str, email:str, password:str, pass_confirm:str, direccion:str, rol:str='arrendatario', telefono:str=None) -> bool:
     if password != pass_confirm:
-        # messages.error(request, 'Las contraseñas no coinciden')
+        messages.error(request, 'Las contraseñas no coinciden')
         return False
     try:
         user = User.objects.create_user(
@@ -58,16 +60,17 @@ def crear_user(username:str, first_name:str, last_name:str, email:str, password:
             last_name=last_name,
         )
     except IntegrityError:
-        # messages.error(request, 'El rut ya está ingresado')
+        messages.error(request, 'El rut ya está ingresado')
         return False
     UserProfile.objects.create(
         direccion=direccion,
         telefono_personal=telefono,
-        rol = rol,
+        rol=rol,
         user=user
     )
-    # messages.success(request, 'Usuario creado con éxito! Por favor, ingrese')
+    messages.success(request, 'Usuario creado con éxito! Por favor, ingrese')
     return True
+
 
 def eliminar_inmueble(inmueble_id):
     inmueble_encontrado = Inmueble.objects.get(id=inmueble_id)
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 # main/urls.py
 from django.urls import path
-from . import views
+from . import views  # Import views from the current package
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/register/', views.register, name='register'),  # Use views.register
 ]
